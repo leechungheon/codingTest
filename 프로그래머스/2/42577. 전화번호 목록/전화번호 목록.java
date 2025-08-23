af43c35ace83da9b671f142129030e75953e3381
ChungheonLee <chung4111@naver.com>
2025-08-23 19:21:28 +0900

    [level 2] Title: 전화번호 목록, Time: 322.23 ms, Memory: 100 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,17 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Map<String, Integer> map=new HashMap<>();
+        // 전화번호부를 사전식으로 오름차순 정렬
         Arrays.sort(phone_book);
-        for(int i=0; i<phone_book.length; i++){
-            map.put(phone_book[i],i);
-        }
-        for(int i=0; i<phone_book.length; i++){
-            for(int j=1; j<phone_book[i].length(); j++){
-                if(map.containsKey(phone_book[i].substring(0,j))){
-                    answer=false;
-                };
-                if(!answer)break;
+
+        // 정렬된 배열을 순회하며 바로 다음 번호와 비교
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            // 현재 번호(phone_book[i])가 바로 다음 번호(phone_book[i+1])의 접두어인지 확인
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false; // 접두어가 있으면 즉시 false 반환
             }
-            if(!answer)break;
         }
-        return answer;
+        return true; // 전체를 다 확인해도 접두어가 없으면 true 반환
     }
 }
