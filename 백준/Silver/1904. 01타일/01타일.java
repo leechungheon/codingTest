5161b994c47d164b9c3c751813505c5fe4d75684
ChungheonLee <chung4111@naver.com>
2025-12-23 20:07:31 +0900

    [Silver III] Title: 01타일, Time: 116 ms, Memory: 18104 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Main
+{
+	static int N;
+	static int cnt = 0;
+	static int[] dp = new int[1000001];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		dp[1]=1;
+		dp[2]=2;
+		for(int i=3; i<=N; i++) {
+			dp[i]=(dp[i-1]+dp[i-2])%15746;
+		}
+		System.out.println(dp[N]);
+	}
+}
