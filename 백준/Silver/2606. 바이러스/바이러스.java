1b4ecbbf266cd5284805a685473e3b4a49349c48
ChungheonLee <chung4111@naver.com>
2026-02-27 20:33:46 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,38 +1,45 @@
 import java.util.*;
 import java.io.*;
-
-class Main {
-    public static void main(String[] args) throws Exception {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	int numberOfComputers = Integer.parseInt(br.readLine());
-    	int numberOfLines = Integer.parseInt(br.readLine());
-    	int[][] lines = new int[numberOfComputers+1][numberOfComputers+1];
-    	boolean[] visited = new boolean[numberOfComputers+1];
-    	
-    	for(int i=0; i<numberOfLines; i++) {
-    		StringTokenizer st = new StringTokenizer(br.readLine());
-    		int x = Integer.parseInt(st.nextToken());
-    		int y = Integer.parseInt(st.nextToken());
-    		lines[x][y]=1;
-    		lines[y][x]=1;
-    	}
-    	
-    	Queue<Integer> q = new LinkedList<>();
-    	q.add(1);
-    	visited[1] = true;
-    	while(!q.isEmpty()) {
-    		int node = q.poll();
-    		for(int i=1; i<numberOfComputers+1; i++) {
-    			if(lines[node][i]==1 && !visited[i]) {
-    				q.add(i);
-    				visited[i] = true;
-    			}
-    		}
-    	}
-    	int count = 0;
-    	for(int i=1; i<numberOfComputers+1; i++) {
-    		if(visited[i])count++;
-    	}
-    	System.out.println(count-1);
-    }
+class Main
+{
+	public static void main(String args[]) throws Exception
+	{
+		int answer = 0;
+		
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int com = Integer.parseInt(br.readLine());
+        boolean[] visited = new boolean[com+1];
+        visited[1] = true;
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        
+        int[][] adj = new int[com+1][com+1];
+        int line = Integer.parseInt(br.readLine());
+        
+        for(int i=0; i<line ;i++) {
+        	StringTokenizer st = new StringTokenizer(br.readLine());
+        	int x = Integer.parseInt(st.nextToken());
+        	int y = Integer.parseInt(st.nextToken());
+        	
+        	adj[x][y] = 1;
+        	adj[y][x] = 1;
+        }
+        
+        while(!q.isEmpty()) {
+        	int n = q.poll();
+        	for(int i=1; i<com+1; i++) {
+        		if(adj[n][i] ==1 && !visited[i]) {
+        			q.add(i);
+        			visited[i] = true;
+        		}
+        	}
+        }
+        
+        for(int i=2; i<com+1; i++) {
+        	if(visited[i])answer++;
+        }
+        
+        System.out.print(answer);
+	}
 }
